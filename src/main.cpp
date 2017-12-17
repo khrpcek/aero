5e0fa357e0d786e67089832df9a8df5e99ad5d12
Kevin Hrpcek <khrpcek@gmail.com>
2017-12-17 13:28:54 -0600

    This works and is running. Does not contain pump auto off based on run time

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,132 @@
+// PINS: float switch = d2, pump relay = d3, pressure transducer = a1, solenoind = d4
+// this should run on a nano or uno, but does nout output anything
+//
+
 #include <Arduino.h>
+#include <Wire.h>
+
+//Variables for pressure switch
+int floatSwitchPin = 2; //float switch digital pin
+boolean lowWater; //Boolean for float sensor
+int pswRelayPin = 3; //relay digital pin
+int psiTransPin = A1; //
+int lowLimit = 65; //psi low lowLimit
+int highLimit = 120; //psi high limit
+float psi = 0; //initiaize psi
+float avgPSI = 180; //start the psi high to not activate the pump on boot
+float combinedPSI = 0; //float for adding psi together before dividing for avgPSI
+boolean pumpState = false; //boolean for if the relay is active or not. start as not
+unsigned long psiReadInterval = 1000; //interval for reading psi in the smoothing loop
+int pswSmoothCount = 10; //smooth psi readings over x psiReadInterval
+int pswSmoothCurCount = 0; //Counter for keeping track of smooth interations
+
+//Variables for mister  //Activate relay if psi is too low and flot returns 0
+
+int misterRelayPin = 4;
+boolean misterState = false;
+unsigned long mistInterval = 5; //mist active for X minutes, converts to millis later
+unsigned long mistAct = 4; //mist for Y seconds
+
+//Timers for pressure switch
+unsigned long pswIntervalPrev = 0; //millis since last psw activation
+unsigned long psiSmoothTimer = 0; //counts to psiReadInterval for smoothing
+unsigned long psiIntervalTimer = 0; //timer for psi smoothing
+unsigned long pumpRunTimer = 0; //counts time pump is running to not exceed X min
+unsigned long pumpLastStartTimer = 0; //used to count time between pump runs
+
+
+//Timers for mister
+unsigned long mistIntervalPrev = 0; //millis since last mist activation
+unsigned long mistActiveTimer = 0; //timer for keeping mist active
+
+
+//Function to return current psi reading
+float readPressure() {
+  //psi transducer returns a value 0-1023 which needs to be converted in a voltage
+  //Voltage returned needs to be 0.5-5.0v and is converted to psi
+  //to get the 0.03 divide the transducer max psi by the voltage range
+  //so 150 psi / 4.5v = 0.03, the 1 psi is equal to 0.03v
+
+  int psiSensorRaw = analogRead(psiTransPin);
+  float voltage = psiSensorRaw * (5.0 / 1023.0);
+  float psi = (voltage - 0.5) / 0.03;
+  return psi;
+}
+//function to return float switch status
+boolean floatStatus() {
+  if (digitalRead(floatSwitchPin) == LOW) {
+    //Low means sensor is floating
+    lowWater = false;
+  } else if (digitalRead(floatSwitchPin) == HIGH) {
+    //High means sensor is not floating
+    lowWater = true;
+  }
+  return lowWater;
+}
+
 
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(9600);
+  //Setup Pins
+  pinMode(pswRelayPin, OUTPUT); //pressure switch relay
+  pinMode(floatSwitchPin, INPUT_PULLUP); //float pin set for reading
+  pinMode(misterRelayPin, OUTPUT); //mister solenoid relay
+    // put your main code here, to run repeatedly:
+  //For the relays, HIGH is not active, LOW is active
+  //Set relays to high at beginning of program so they don't mist
+  digitalWrite(pswRelayPin, HIGH);
+  digitalWrite(misterRelayPin, HIGH);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+  //Get current millis and store it for this loop
+  unsigned long currentMillis = millis();
+
+  //BEGIN MISTER ACTIVATION
+
+  if ((currentMillis - mistIntervalPrev) >= (mistInterval * 60 * 1000)) {
+    digitalWrite(misterRelayPin, LOW);
+    mistIntervalPrev = currentMillis;
+    mistActiveTimer = currentMillis;
+    misterState = true;
+  }
+  else if ((currentMillis - mistActiveTimer) >= (mistAct * 1000) && (misterState == true)) {
+    digitalWrite(misterRelayPin, HIGH);
+    misterState = false;
+  }
+//END MISTER ACTIVATION
+
+//BEGIN PRESSURE SWITCH
+
+//Average the psi reading over multiple loops to smooth it
+  if ((unsigned long)(currentMillis - psiSmoothTimer) >= psiReadInterval) {
+    float psi = readPressure();
+    combinedPSI = combinedPSI + psi;
+    if (pswSmoothCurCount == pswSmoothCount) {
+      avgPSI = combinedPSI/pswSmoothCount;
+      pswSmoothCurCount = 0;
+      combinedPSI = 0;
+      psiSmoothTimer = currentMillis;
+    } else {
+      pswSmoothCurCount = pswSmoothCurCount + 1;
+    }
+  }
+
+
+  //Get float switch status
+  lowWater = floatStatus();
+
+  //Activate relay if psi is too low and flot returns 0
+  //if ((avgPSI < lowLimit) && (pumpState == false) && (lowWater == false) && ((currentMillis - pumpLastStartTimer) > 600000))  {
+  if ((avgPSI < lowLimit) && (pumpState == false) && (lowWater == false))  {
+    digitalWrite(pswRelayPin, LOW);
+    pumpLastStartTimer = currentMillis; //set time the pump last turned on
+    pumpRunTimer = currentMillis; //set time pump turned on to make sure it doesn't exceed max runtime
+    pumpState = true;
+  //} else if (((avgPSI > highLimit) || (lowWater = true)) && pumpState == true) || ((currentMillis - pumpRunTimer) > 300000)) {
+  } else if ((avgPSI > highLimit) || ((pumpState == true) && (lowWater == true))) {
+    digitalWrite(pswRelayPin, HIGH);
+    pumpState = false;
+  }
+  ///END PRESSURE SWITCH
 }
